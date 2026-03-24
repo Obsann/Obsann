494e55544161fa412bf23c25413fb501e5e27c3f
Obsann <obsanhabtamu0@gmail.com>
2026-03-24 23:48:04 +0300

    touch scrolling issue solved

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const lenis = new Lenis({
   duration: 1.4,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   smoothWheel: true,
-  smoothTouch: true,
+  smoothTouch: false,
   wheelMultiplier: 0.8,
   touchMultiplier: 2,
 });
